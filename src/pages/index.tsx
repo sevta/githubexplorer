ea8ea3f110ac1bb27705fb8c13b38f8156eb77d5
septa <sevtasaputra@gmail.com>
2023-01-16 16:56:41 +0700

    fix: remapping user & repos into 1 record

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,24 +29,38 @@ type Repo = {
   stargazers_count: number;
 } & Record<any, any>;
 
+type Result = {
+  user: User;
+  repo: Repo[];
+};
+
 export default function Home() {
   const [searchKey, setSearchKey] = useInputState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
-  const [data, setData] = useState<User[]>([]);
-  const [repos, setRepos] = useState<Repo[]>([]);
+  const [data, setData] = useState<Result[]>([]);
 
   async function submitSearch() {
     setLoading(true);
 
     try {
+      let res: Result[] = [];
       const resp = await axios.get(api + "/search/users?q=" + searchKey);
-      setData(resp?.data.items?.slice(0, 5));
-      if (resp?.data) {
-        const getRepos = await axios.get(
-          api + "/users/" + searchKey + "/repos"
-        );
-        setRepos(getRepos?.data);
-      }
+      const result = resp?.data?.items?.slice(0, 5);
+      result.map((r: any, index: number) => {
+        res.push({
+          user: r,
+          repo: [],
+        });
+      });
+      await Promise.all(
+        result.map(async (r: any, index: number) => {
+          const getRepos = await axios.get(
+            api + "/users/" + r.login + "/repos"
+          );
+          res[index].repo = getRepos.data;
+        })
+      );
+      setData(res);
     } catch (error: any) {
       showNotification({ color: "red", message: error?.message || "error" });
     } finally {
@@ -76,30 +90,26 @@ export default function Home() {
       </Stack>
       {data?.map((d, i) => (
         <Accordion variant="filled" mt="sm">
-          <Accordion.Item value={d?.id?.toString() || "1"} px={0}>
+          <Accordion.Item value={d.user?.id?.toString() || "1"} px={0}>
             <Accordion.Control>
               <Text weight={600} size="lg">
-                {d?.login || "-"}
+                {d?.user.login || "-"}
               </Text>
             </Accordion.Control>
             <Accordion.Panel>
               <Stack>
-                {repos.length === 0 ? (
-                  <Text>No repos</Text>
-                ) : (
-                  repos?.map((repo, indexRepo: number) => (
-                    <Paper withBorder py="md" px="xs" bg="gray.3">
-                      <Flex justify="space-between">
-                        <Text weight={600}>{repo?.name}</Text>
-                        <Group>
-                          <Text weight={600}>{repo?.stargazers_count}</Text>
-                          <IconStar size={14} />
-                        </Group>
-                      </Flex>
-                      <Text size="sm">{repo?.description}</Text>
-                    </Paper>
-                  ))
-                )}
+                {d.repo?.map((repo, indexRepo: number) => (
+                  <Paper withBorder py="md" px="xs" bg="gray.3">
+                    <Flex justify="space-between">
+                      <Text weight={600}>{repo?.name}</Text>
+                      <Group>
+                        <Text weight={600}>{repo?.stargazers_count}</Text>
+                        <IconStar size={14} />
+                      </Group>
+                    </Flex>
+                    <Text size="sm">{repo?.description}</Text>
+                  </Paper>
+                ))}
               </Stack>
             </Accordion.Panel>
           </Accordion.Item>
